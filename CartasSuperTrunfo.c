0489b31af08e126eb503b4041a979296cfddc76a
Amanda F Garcia <amandafgarcia25@gmail.com>
2025-04-05 21:32:24 -0300

    Update CartasSuperTrunfo.c

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,25 +1,46 @@
 #include <stdio.h>
 
-int main() {
-    printf("DESAFIO SUPER TRUNFO DE PAÍSES.\n");
+   int main(){
 
-    // Data for cards
-    char *codigos[] = {"A", "B"};
-    char *nomes[] = {"Rio de Janeiro", "Salvador"};
-    int populacoes[] = {6729894, 2500000}; // Corrected to integer values
-    float areas[] = {120033, 693442};
-    float pibs[] = {949301, 62954.0}; // Corrected to float values
-    int pontos_turisticos[] = {58, 25};
+    ////// carta a
 
-    printf("Por favor, insira o código da sua carta (A ou B):\n");
-    char codigo_selecionado;
-    scanf(" %c", &codigo_selecionado);
+    char estadoA[50] = "Rio de Janeiro";
+    char códigoA [5] = "A01";
+    char nomeA[50] = "Rio de Janeiro";
+    float populacaoA = 16055174;
+    float areaA = 1.200;
+    double pibA = 300.50;
+    int pontos_turísticosA = 58;
 
-    // Determine the index based on the selected code
-    int index = (codigo_selecionado == 'A') ? 0 : 1;
 
-    printf("Os dados da carta são: código: %s, nome: %s, população: %d, área: %.2f, PIB: %.2f, pontos turísticos: %d.\n",
-           codigos[index], nomes[index], populacoes[index], areas[index], pibs[index], pontos_turisticos[index]);
+    ////// carta b
+
+    char estadoB[50] = "São Paulo";
+    char códigoB [5] = "A02";
+    char nomeB[50] = "São Paulo";
+    float populacaoB = 11451999;
+    float areaB = 1.521202;
+    double pibB = 2.719751;
+    int pontos_turísticosB = 30;
+
+    ///////// exibição dos dados das cartas /////////
+
+    printf("Estado 1: %s\n", estadoA);
+    printf("Código 1: %s\n", códigoA);
+    printf("Nome 1: %s\n", nomeA);
+    printf("População 1: %.2f\n", populacaoA);
+    printf("Área 1: %.2f\n", areaA);
+    printf("PIB 1: %.2f\n", pibA);
+    printf("Pontos turisticos 1: %d\n", pontos_turísticosA);
+
+    printf("Estado 2: %s\n", estadoB);    
+    printf("Código 2: %s\n", códigoB);
+    printf("Nome 2: %s\n", nomeB);    
+    printf("População 2: %.2f\n", populacaoB);
+    printf("Área 2: %.2f\n", areaB);
+    printf("PIB 2: %.2f\n", pibB);
+    printf("Pontos turisticos 2: %d\n", pontos_turísticosB);
 
     return 0;
-}
+
+   }
